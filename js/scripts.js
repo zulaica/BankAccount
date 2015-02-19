835640c426a0e0d350c10c72505a7001440be8af
David Zulaica <zulaica@gmail.com>
2015-02-18 17:28:39 -0800

    Refactor.

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,12 @@ var BankAccount = {
 $(document).ready(function(){
   var newAccount = Object.create(BankAccount);
 
-  $('form#create_account').submit(function(event) {
+  $('#create_account').submit(function(event) {
     event.preventDefault();
 
-    var nameInput = $('input#name').val();
-    var initial_depositInput = parseFloat($('input#initial_deposit').val());
-    isNaN(initial_depositInput) ? initial_depositInput = 0 : initial_depositInput = initial_depositInput;
-
+    var nameInput = $('#name').val();
+    var initial_depositInput = parseFloat($('#initial_deposit').val());
+    if (isNaN(initial_depositInput)) { initial_depositInput = 0; }
 
     newAccount.name = nameInput;
     newAccount.deposit(initial_depositInput);
@@ -29,32 +28,29 @@ $(document).ready(function(){
     $('#manage').toggle("slide", 325).dequeue().hide().fadeIn(500);
   });
 
-  $('form#account_management').submit(function() {
+  $('#account_management').submit(function() {
     event.preventDefault();
 
-    var depositInput = parseFloat($('input#deposit').val());
-    var withdrawInput = parseFloat($('input#withdraw').val());
-    isNaN(depositInput) ? depositInput = 0 : depositInput = depositInput;
-    isNaN(withdrawInput) ? withdrawInput = 0 : withdrawInput = withdrawInput;
+    var depositInput = parseFloat($('#deposit').val());
+    var withdrawInput = parseFloat($('#withdraw').val());
+    if (isNaN(depositInput)) { depositInput = 0; }
+    if (isNaN(withdrawInput)) { withdrawInput = 0; }
 
     var currentBalance = newAccount.balance;
+    var balanceColor = "";
 
     newAccount.deposit(depositInput);
     newAccount.withdraw(withdrawInput);
 
-    if (currentBalance < newAccount.balance) {
-      var balanceColor = "#45a742";
+    if (currentBalance <= newAccount.balance) {
+      balanceColor = "#45a742";
     } else {
-      var balanceColor = "red";
+      balanceColor = "red";
     }
 
     $('.balance').text(newAccount.balance.toFixed(2));
-    $('.balance').animate({
-      color: balanceColor,
-    }, 62.5 );
-    $('.balance').animate({
-      color: "black",
-    }, 825 );
+    $('.balance').animate({ color: balanceColor }, 62.5 );
+    $('.balance').animate({ color: "black" }, 825 );
     $('#deposit').val("");
     $('#withdraw').val("");
   });
